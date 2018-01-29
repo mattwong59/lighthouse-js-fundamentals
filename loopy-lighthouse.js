814b0e034b6eb8aabc3271ccc360534c4442c1e2
Matt Wong <mattwong59@gmail.com>
2018-01-29 22:29:39 +0000

    Adds refacturing of loopy-lighthouse with loopy-lighthouse2 solution

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,16 +1,23 @@
-for(var num = 100; num <= 200; num++) {
-  if(num % 3 === 0 && num % 4 === 0) {
-    console.log("LoopyLighthouse");
-  }
-  else if(num % 4 === 0) {
-    console.log("Lighthouse");
-  }
-  else if(num % 3 === 0) {
-    console.log("Loopy");
-  }
-  else{
-    console.log(num);
+function loopyLighthouse (range, multiples, words) {
+//range represents the start and end of a range of numbers (2 numbers)
+//multiples represents the multiples to be replaced with words (2 numbers)
+//words are replacing the values of the multiples
+
+  for(var num = range[0]; num <= range[1]; num++) {
+    if(num % multiples[0] === 0 && num % multiples[1] === 0) {
+      console.log(words.join(""));
+    }
+    else if(num % multiples[0] === 0) {
+      console.log(words[0]);
+    }
+    else if(num % multiples[1] === 0) {
+      console.log(words[1]);
+    }
+    else{
+      console.log(num);
+    }
   }
 }
 
+loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
 
